b6ed2f88430a27eb246afeaecb1741e2fbd32934
Aman Gupta <2015aman.a@ves.ac.in>
2020-08-11 17:27:51 +0530

    consuming ccontext in two different ways

--- contectapp/src/App.js
+++ contectapp/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from './component/Navbar'
 import Booklist from './component/Booklist'
+import ThemeContextProvider from './contexts/ThemeContextProvider';
+import ThemeToggle from './component/ThemeToggle';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Booklist />
+      <ThemeContextProvider>
+        <Navbar />
+        <Booklist />
+        <ThemeToggle />
+      </ThemeContextProvider>
     </div>
   );
 }
