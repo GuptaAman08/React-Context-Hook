156e1c1513aad7a7fdbba61d91d90a011064ff7f
Aman Gupta <2015aman.a@ves.ac.in>
2020-08-17 11:34:40 +0530

    Adding book context provider and using useReducer hook

--- BookListApp/booklist/src/contexts/BookContextProvider.js
+++ BookListApp/booklist/src/contexts/BookContextProvider.js
@@ -1,25 +1,21 @@
-import React, { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { createContext, useReducer, useEffect } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
+
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-    const [ books, setBooks ] = useState([
-        {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-        {title: 'the final empire', author: 'brandon sanderson', id: 2}
-    ])
+    const [books, dispatch ] = useReducer(bookReducer, [], () => {
+        const data = localStorage.getItem('books')
+        return data ? JSON.parse(data) : []
+    })
 
-    const addBook = (title, author) => {
-        setBooks([...books, { title, author, id: uuidv4()}])
-    }
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books])
 
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id))
-    }
-
-    console.log(books)
     return (  
-        <BookContext.Provider value={{ books, addBook, removeBook}}> 
+        <BookContext.Provider value={{ books, dispatch}}> 
             {props.children}
         </BookContext.Provider>
     );
